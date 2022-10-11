d050f2f1bf4b5deec2e27c141aa966e5a2b0017e
jh9219 <107241050+kk8712@users.noreply.github.com>
2022-10-11 09:45:21 +0900

    1
    1

--- TestProject/src/app/MainClass.java
+++ TestProject/src/app/MainClass.java
@@ -2,6 +2,7 @@ package app;
 
 public class MainClass {
 	public static void main(String[] args) {
-		System.out.println("충돌 테스트용 프로그램입니다.");
+		System.out.println("충돌 테스트용 프로그램입니다.cuga");
+		System.out.println("cnrk");
 	}
 }
